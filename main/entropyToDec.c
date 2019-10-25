bb0f224f60766f18fa2b46aba767925903a3d8e4
Evann <evanndebailliencourt83@gmail.com>
2019-10-25 18:50:12 +0200

    modif main.c + add bitArrayToDecArray function

--- main/entropyToDec.c
+++ main/entropyToDec.c
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <memory.h>
+
+
+void display(int* aff, int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%d\t", aff[i]);
+	}
+}
 
 
 
@@ -17,9 +25,17 @@ int bitToDec(int* bits, unsigned int size) {
 			ret = ret * 2 + 1;
 		}
 		else{ret *= 2;}
-			
 	}
-
 	return ret;
+}
+
+int* bitArrayToDecArray(int bitArray[], int arraySize) {
+	int* decArray = (int*)malloc(sizeof(int) * 11);
+
+	for (int i = 0; i < arraySize; i++) {
+			decArray[i] = bitToDec(bitArray[i], 11);
+		
+	}
+	return decArray;
 }
 
--- main/main.c
+++ main/main.c
@@ -17,8 +17,18 @@ int main(int argc, char** argv) {
 	printf_s("(1)---> entropy to mnemonic/seed\n(2)---> mnemonic to entropy/seed\n(3)--->mnemonic/seed validation\n");
 
 	int ret = 0;
-	int bit[11] = { 0,1,1,0,0,1,0,1,1,0,1 };
-	ret = bitToDec(bit, 11);
-	printf("%d", ret);
+	int ret2 = 0;
+	int *dec=(int*)malloc(sizeof(int)*2);
+	int bit[2][11] = { {0,1,1,0,0,1,0,1,1,0,1},
+					   {0,0,0,0,0,1,1,1,0,0,0} };
+
+	ret = bitToDec(bit[0], 11);
+	ret2 = bitToDec(bit[1], 11);
+
+	printf("ret = %d\tret2 = %d", ret, ret2);
+	dec = bitArrayToDecArray(bit,2);
+	display(dec);
+
+
 	return 0;
 }
