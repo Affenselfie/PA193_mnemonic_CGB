ef26b2bcea11ded014e8645a2482389177f47cd7
Evann <evanndebailliencourt83@gmail.com>
2019-10-25 17:47:23 +0200

    REINIT

---
+++ main/main.c
@@ -0,0 +1,19 @@
+/** PROJECT SECURE CODING BIP39
+
+AUTHORS: DE BAILLIENCOURT Evann / HAMAILI Stéphane
+
+
+**/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+
+	printf_s("============ PROJECT SECURE BIP39 ============\n");
+	printf_s("Choose :\n");
+	printf_s("(1)---> entropy to mnemonic/seed\n(2)---> mnemonic to entropy/seed\n(3)--->mnemonic/seed validation\n");
+
+	return 0;
+}
