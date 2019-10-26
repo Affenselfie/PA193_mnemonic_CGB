cbf2e04a04eab000a4d649a3e0b7bd2deb1df158
Evann <evanndebailliencourt83@gmail.com>
2019-10-26 15:43:45 +0200

    fixing bug and finish conv binaire dec and reverse

--- main/convNumber.c
+++ main/convNumber.c
@@ -10,13 +10,27 @@
 #define SIZE_ARRAY 2
 
 
-void setRandDec(int* tab, size_t size) {
+
+void setRandDec(int* decTab, size_t size) {
 	time_t t;
 	/* Intializes random number generator */
 	srand((unsigned)time(&t));
 
 	for (unsigned int i = 0; i < 11; i++) {
-		tab[i] = rand() % 2048;
+		decTab[i] = rand() % 2048;
+	}
+}
+
+void setRandBin(int** binTab, size_t size) {
+	time_t t;
+	/* Intializes random number generator */
+	srand((unsigned)time(&t));
+
+	for (unsigned int i = 0; i < size; i++) {
+		for (unsigned int j = 0; j < size; j++)
+		{
+			binTab[i][j] = rand() % 2;
+		}
 	}
 }
 
@@ -25,9 +39,19 @@ void display(int* aff, size_t size) {
 	for (int i = 0; i < size; i++) {
 		printf("|\t%d\t", aff[i]);
 	}
+	printf("\n");
+}
+void displayBin(int** aff, size_t sizeh, size_t sizel) {
+	for (int i = 0; i < sizeh; i++) {
+		for (unsigned int j = 0; j < sizel; j++) {
+			printf("|%d", aff[i][j]);
+		}
+		printf("\n");
+
+	}
 }
 
-int bitToDec(int* bits, size_t size) {
+int binToDec(int* bits, size_t size) {
 	int ret = 0;
 	for (unsigned int i = 0; i < size; i++)
 	{
@@ -36,16 +60,15 @@ int bitToDec(int* bits, size_t size) {
 			ret = ret * 2 + 1;
 		}
 		else { ret *= 2; }
-
-		printf("\t%d\n", ret);
 	}
 	return ret;
 }
 
-void bitArrayToDecArray(int* tab, size_t size, int* decArray) {
+void binArrayToDecArray(int** binArray, size_t size, int* decArray) {
 
-	*decArray = bitToDec(tab, 11);
-	printf("dec[0]= %d", *decArray);
+	for (int i = 0; i < size; i++) {
+		decArray[i] = binToDec(binArray[i], 11);
+	}
 
 }
 
@@ -58,11 +81,10 @@ int* decToBin(int nbr, int* nbrBin) {
 		tab[i] = nbr % 2;
 		nbr = nbr / 2;
 	}
-	printf("\nLe nombre binaire est = ");
+	//printf("\nLe nombre binaire est = ");
 	for (i = i - 1; i >= 0; i--)
 	{
 		nbrBin[i] = tab[i];
-		printf("%d", nbrBin[i]);
 	}
 
 	return nbrBin;
--- main/main.c
+++ main/main.c
@@ -14,11 +14,6 @@ AUTHORS: DE BAILLIENCOURT Evann / HAMAILI Stéphane
 
 
 int main(int argc, char** argv) {
-
-
-
-
-
 	printf("============ PROJECT SECURE BIP39 ============\n");
 	printf("Choose :\n");
 	printf("(1)---> entropy to mnemonic/seed\n(2)---> mnemonic to entropy/seed\n(3)--->mnemonic/seed validation\n\n\n");
@@ -26,24 +21,25 @@ int main(int argc, char** argv) {
 	int ret = 0;
 	int ret1 = 0;
 
-	int tabDec[11];
-
-	int decimal[2];
-	int bit[2][11] = { {0,1,1,0,0,1,0,1,1,0,1},
-					   {0,0,0,0,0,1,1,1,0,0,0} };
+	int* tabDec = (int*)malloc(sizeof(int) * 11);
 
 	int** bin = (int**)malloc(sizeof(int*) * 11);
-
 	for (int i = 0; i < 11; i++) {
 		bin[i] = (int*)malloc(sizeof(int) * 11);
+		memset(bin[i], 0, sizeof(bin[i]));
 	}
 
-
-	setRandDec(tabDec, 11);
+	setRandBin(bin, 11);
+	displayBin(bin, 11, 11);
+	binArrayToDecArray(bin, 11, tabDec);
 	display(tabDec, 11);
 
-	//bitArrayToDecArray(bit, 2, decimal);
+	/*setRandDec(tabDec, 11);
+	display(tabDec, 11);
 	decArrayToBinArray(tabDec, 11, bin);
+	displayBin(bin, 11,11);*/
+
+
 
 
 
