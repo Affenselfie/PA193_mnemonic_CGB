885261084df4fca2de88b697c4f61c47d81c45d2
Evann <evanndebailliencourt83@gmail.com>
2019-10-28 15:19:45 +0100

    fix + add function for conversion number

--- main/convNumber.c
+++ main/convNumber.c
@@ -6,9 +6,8 @@
 #include <math.h>
 #include <memory.h>
 
-#define SIZE_BITS 11
-#define SIZE_ARRAY 2
 
+#define SIZE_BITS 11
 
 
 void setRandDec(int* decTab, size_t size) {
@@ -37,11 +36,13 @@ void setRandBin(int** binTab, size_t size) {
 
 void display(int* aff, size_t size) {
 	for (int i = 0; i < size; i++) {
-		printf("|\t%d\t", aff[i]);
+		printf("|%d", aff[i]);
 	}
 	printf("\n");
 }
+
 void displayBin(int** aff, size_t sizeh, size_t sizel) {
+	printf("\n");
 	for (int i = 0; i < sizeh; i++) {
 		for (unsigned int j = 0; j < sizel; j++) {
 			printf("|%d", aff[i][j]);
@@ -52,22 +53,20 @@ void displayBin(int** aff, size_t sizeh, size_t sizel) {
 }
 
 int binToDec(int* bits, size_t size) {
+	float puissance = 0.5;
 	int ret = 0;
 	for (unsigned int i = 0; i < size; i++)
 	{
-		if (bits[(size - 1) - i] == 1)
-		{
-			ret = ret * 2 + 1;
-		}
-		else { ret *= 2; }
+		ret += bits[size - 1 - i] * 2 * puissance;
+		puissance *= 2;
 	}
 	return ret;
 }
 
-void binArrayToDecArray(int** binArray, size_t size, int* decArray) {
+void binArrayToDecArray(int** binArray, size_t size, size_t sizeBin, int* decArray) {
 
 	for (int i = 0; i < size; i++) {
-		decArray[i] = binToDec(binArray[i], 11);
+		decArray[i] = binToDec(binArray[i], sizeBin);
 	}
 
 }
@@ -95,3 +94,135 @@ void decArrayToBinArray(int* decArray, size_t size, int** binArray) {
 		binArray[i] = decToBin(decArray[i], binArray[i]);
 	}
 }
+
+void cpyTab(int* dest, int* src, size_t size) {
+	for (unsigned int i = 0; i < size; i++) {
+		dest[i] = src[i];
+	}
+}
+
+int* HexToBin(char* hexdec)
+{
+	int* bin = (int*)malloc(sizeof(int) * 128);
+	memset(bin, 0, sizeof(bin));
+
+	int tabHex[16][4] = {
+	{0,0,0,0},
+	{0,0,0,1},
+	{0,0,1,0},
+	{0,0,1,1},
+	{0,1,0,0},
+	{0,1,0,1},
+	{0,1,1,0},
+	{0,1,1,1},
+	{1,0,0,0},
+	{1,0,0,1},
+	{1,0,1,0},
+	{1,0,1,1},
+	{1,1,0,0},
+	{1,1,0,1},
+	{1,1,1,0},
+	{1,1,1,1} };
+
+	long int i = 0;
+	int  j = 0;
+
+
+	for (i = 0; i < strlen(hexdec); i++)
+	{
+
+		switch (hexdec[i]) {
+		case '0':
+			cpyTab(&bin[j], tabHex[0], 4);
+			break;
+		case '1':
+			cpyTab(&bin[j], tabHex[1], 4);
+			break;
+		case '2':
+			cpyTab(&bin[j], tabHex[2], 4);
+			break;
+		case '3':
+			cpyTab(&bin[j], tabHex[3], 4);
+			break;
+		case '4':
+			cpyTab(&bin[j], tabHex[4], 4);
+			break;
+		case '5':
+			cpyTab(&bin[j], tabHex[5], 4);
+			break;
+		case '6':
+			cpyTab(&bin[j], tabHex[6], 4);
+			break;
+		case '7':
+			cpyTab(&bin[j], tabHex[7], 4);
+			break;
+		case '8':
+			cpyTab(&bin[j], tabHex[8], 4);
+			break;
+		case '9':
+			cpyTab(&bin[j], tabHex[9], 4);
+			break;
+		case 'A':
+		case 'a':
+			cpyTab(&bin[j], tabHex[10], 4);
+			break;
+		case 'B':
+		case 'b':
+			cpyTab(&bin[j], tabHex[11], 4);
+			break;
+		case 'C':
+		case 'c':
+			cpyTab(&bin[j], tabHex[12], 4);
+			break;
+		case 'D':
+		case 'd':
+			cpyTab(&bin[j], tabHex[13], 4);
+			break;
+		case 'E':
+		case 'e':
+			cpyTab(&bin[j], tabHex[14], 4);
+			break;
+		case 'F':
+		case 'f':
+			cpyTab(&bin[j], tabHex[15], 4);
+			break;
+		default:
+			printf("\nInvalid hexadecimal digit");
+		}
+		j = j + 4;
+	}
+
+	return bin;
+}
+
+void split(int** dest, int* src, int* cs, size_t size) {
+	unsigned int j = 0;
+	for (unsigned int i = 0; i < size; i++) {
+		cpyTab(dest[i], &src[j], 11);
+		j += 11;
+	}
+	cpyTab(cs, &src[j], 7);
+}
+
+void unsplit(int* dest, int** src, size_t size) {
+	unsigned int j = 0;
+
+	for (unsigned int i = 0; i < size; i++) {
+		cpyTab(&dest[j], src[i], size);
+		j += size;
+	}
+	display(dest, 128);
+}
+
+void binToHex(int* bin) {
+
+	int tmp[4];
+
+	for (unsigned int i = 0; i < 128; i = i + 4) {
+		cpyTab(tmp, &bin[i], 4);
+		printf("%x", binToDec(tmp, 4));
+	}
+	printf("\n");
+}
+
+
--- main/main.c
+++ main/main.c
@@ -14,14 +14,20 @@ AUTHORS: DE BAILLIENCOURT Evann / HAMAILI Stéphane
 
 
 int main(int argc, char** argv) {
-	printf("============ PROJECT SECURE BIP39 ============\n");
+	printf("\n\n============ PROJECT SECURE BIP39 ============\n");
 	printf("Choose :\n");
-	printf("(1)---> entropy to mnemonic/seed\n(2)---> mnemonic to entropy/seed\n(3)--->mnemonic/seed validation\n\n\n");
+	printf("(1)---> entropy to mnemonic/seed\n(2)---> mnemonic to entropy/seed\n(3)---> mnemonic/seed validation\n\n\n");
 
-	int ret = 0;
-	int ret1 = 0;
 
 	int* tabDec = (int*)malloc(sizeof(int) * 11);
+	memset(tabDec, 0, sizeof(tabDec));
+
+	int* tabBin = (int*)malloc(sizeof(int) * 128);
+	memset(tabBin, 0, sizeof(tabBin));
+
+	int* cs = (int*)malloc(sizeof(int) * 7);
+	memset(cs, 0, sizeof(cs));
+
 
 	int** bin = (int**)malloc(sizeof(int*) * 11);
 	for (int i = 0; i < 11; i++) {
@@ -29,19 +35,50 @@ int main(int argc, char** argv) {
 		memset(bin[i], 0, sizeof(bin[i]));
 	}
 
-	setRandBin(bin, 11);
+
+	char* hexInput = (char*)malloc(sizeof(char) * 32);
+
+	hexInput = "9e885d952ad362caeb4efe34a8e91bd2";
+
+	for (unsigned int i = 0; i < strlen(hexInput); i++) {
+		printf("%c", hexInput[i]);
+	}
+	printf("\n\n");
+
+	tabBin = HexToBin(hexInput);
+
+	//display(tabBin, 128);
+
+	split(bin, tabBin, cs, 11);
+
 	displayBin(bin, 11, 11);
-	binArrayToDecArray(bin, 11, tabDec);
+
+	printf("\n\n");
+
+	binArrayToDecArray(bin, 11, 11, tabDec);
+
 	display(tabDec, 11);
 
-	/*setRandDec(tabDec, 11);
+	printf("\nchecksum :\t");
+
+	display(cs, 7);
+
+	printf("\n\n");
+
+	unsplit(tabBin, bin, 11);
+
+	printf("\n\n");
+
+	binToHex(tabBin);
+
+	/*setRandBin(bin, 11);
+	displayBin(bin, 11,11);
+	binArrayToDecArray(bin, 11, tabDec);
+	display(tabDec,11);
+
+	setRandDec(tabDec, 11);
 	display(tabDec, 11);
 	decArrayToBinArray(tabDec, 11, bin);
 	displayBin(bin, 11,11);*/
-
-
-
-
-
 	return 0;
 }
