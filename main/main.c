6078d38f8fe52595435b536e580e294014f2dbc1
EvannDev <52197794+EvannDev@users.noreply.github.com>
2019-10-31 00:43:41 +0100

    fix bugs

--- main/main.c
+++ main/main.c
@@ -5,6 +5,7 @@
 #include <memory.h>
 #include <time.h>
 #include <openssl/sha.h>
+#include <openssl/evp.h>
 #include "Init.h"
 #include "convNum.h"
 #include "stringConv.h"
@@ -13,12 +14,10 @@
 void choice1(char* hexInput){
 
 	int* tabDec = Init(12);
-	int* tabBin = Init(128);
-	int* csF = Init(11);
-	int* cs = Init(7);
-	int** bin= InitTab(11,11);
+	int* tabBin = Init(132);
+	int** bin= InitTab(12,11);
 
-	char *binC=(char*)malloc(sizeof(char)*128);
+	char *binC=(char*)malloc(sizeof(char)*132);
 
 	char** resultChar = (char**)malloc(12 * sizeof(char*));
 	for (int i = 0; i < 12; i++) {
@@ -31,23 +30,17 @@ void choice1(char* hexInput){
 	}
 	printf("\n\n");
 
-	tabBin = HexToBin(hexInput);
+
 
 	printf("Convert binary : ");
 
+	tabBin = HexToBin(hexInput);
+
 	display(tabBin, 128);
 
-	printf("\nSplit in 11 cluster:\n");
 
-	split(bin, tabBin, cs, 11);
 
-	binArrayToDecArray(bin,11,11,tabDec);
-
-	printf("\nInitial checksum :\t");
-
-	display(cs,7);
-
-	printf("\n");
+	printf("\nConvert To String : ");
 
 	char *tmp = (char*)malloc(sizeof(char)*2);
 
@@ -56,7 +49,11 @@ void choice1(char* hexInput){
 		strcat(binC, tmp );
 	}
 
-	printf("binC: %s\n", binC);
+	printf("%s\n", binC);
+
+
+	printf("\n####hashage####");
+
 
 	unsigned char *d = SHA256(binC, strlen(binC), 0);
  	
@@ -65,7 +62,7 @@ void choice1(char* hexInput){
  		string[i] = (char*)malloc(sizeof(char)*4);
  	}
 
- 	printf("\nhashage:");
+ 	printf("\nhashage : ");
 
 	for (unsigned int i = 0; i < SHA256_DIGEST_LENGTH; i++){
 		printf("%02x", d[i]);
@@ -78,25 +75,61 @@ void choice1(char* hexInput){
 
 	int* h = (int*)malloc(sizeof(int)*4);
 
+	printf("\nConvert the first Hex carac : ");
+
 	h=HexToBinC(string[0][0]);
 	printf("\n");
-	cpyTab(csF, h,4);
-	cpyTab(&csF[4], cs, 7);
 
-	printf("Checksum :");
+	display(h,4);
 
-	display(csF, 11);
+	printf("\nChecksum :");
+	
+	cpyTab(&tabBin[128], h,4);
 
-	tabDec[11] = binToDec(csF,11);
+	display(tabBin, 132);
+
+	printf("\nSplit in 11 parts:\n");
+
+	split(bin,tabBin,12,11);
+	displayBin(bin,12,11);
+
+	binArrayToDecArray(bin,12,11,tabDec);
+	
 	printf("\nDecimal Array: ");
 	display(tabDec, 12);
+	printf("\n");
 
 	resultChar = findWord(tabDec,"wordlist.txt", resultChar); 
 
 
 }
 
+void choice2(char ** mnemonic){
 
+	int ** binArray = InitTab(12,11);
+	int ** hexArray = InitTab(32,4);
+	int * decArray = Init(12);
+	int *bin=Init(132);
+	int *hexBin=Init(128);
+
+	decArray = StringToInt("wordlist.txt",mnemonic,decArray);
+	printf("\nDisplay dec : ");
+	display(decArray, 12);
+	printf("\nConvert to Bin :");
+	decArrayToBinArray(decArray,12,binArray);
+	displayBin(binArray, 12, 11);
+	printf("\n\nDisplay Row :");
+	unsplit(bin, binArray, 12);
+	printf("\n\nRemove last 4 bits : ");
+	cpyTab(hexBin,bin,128);
+	display(hexBin,128);
+	printf("\n\nSplit in 32 parts : ");
+	split(hexArray,hexBin,32,4);
+	displayBin(hexArray,32,4);
+	printf("\n\nConvert To Hex : ");
+	binToHex(hexBin,128);
+
+}
 
 int main(int argc, char** argv) {
 
@@ -106,121 +139,19 @@ int main(int argc, char** argv) {
 
 	char* hexInput= (char*)malloc(sizeof(char)*32);
 
-	hexInput = "9e885d952ad362caeb4efe34a8e91bd2";
+	hexInput = "063679ca1b28b5cfda9c186b367e271e";
 
 	choice1(hexInput);
+	
+	printf("\n\nMnemonic :\n");
+
+	char** mnemonic = (char**)malloc(12 * sizeof(char*));
+	for (int i = 0; i < 12; i++) {
+		mnemonic[i] = (char*)malloc(sizeWordMax * sizeof(char));
+		scanf("%s", mnemonic[i]);
+	}
+	choice2(mnemonic);
 
 
 	return 0;
 }
-
-/*
-void viderBuffer()
-{
-	int c = 0;
-	while (c != '\n' && c != EOF)
-	{
-		c = getchar();
-	}
-}
-
-void entropyChoice() {
-	
-
-	while (*ch != 'q') {
-
-		printf("choose 1 or 2 :\n1) binary\nor\n2) hexa\n");
-		fgets(choiceEntropy, 2, stdin);
-		switch (*choiceEntropy) {
-		case '1':
-			viderBuffer();
-			printf("enter entropy:\n");
-			fgets(entropy, 128, stdin);
-			//findWord(/*fonction qui converti binaire en entiers, cheminFichier,);
-			break;
-		case '2':
-			viderBuffer();
-			printf("enter entropy:\n");
-			fgets(entropy, 32, stdin);
-			//findWord(/*fonction qui converti hexa en entiers, cheminFichier);s
-			break;
-		default:
-			printf("Choose 1 or 2 please\n");
-			break;
-		}
-	}
-	free(entropy);
-	free(choiceEntropy);
-}
-
-int main(void) {
-	char *choice = (char)malloc(sizeof(char) * 3);
-	char *mnemonic = (char)malloc(500 * sizeof(char));
-	char *seed = (char)malloc(300 * sizeof(char));
-	char* entropy = (char)malloc(130 * sizeof(char));
-	char* choiceEntropy = (char)malloc(sizeof(char));
-	int* resultInt = (int*)malloc(12 * sizeof(int));
-	char** resultChar = (char**)malloc(12 * sizeof(char*));
-	for (int i = 0; i < 12; i++) {
-		resultChar[i] = (char*)malloc(sizeWordMax * sizeof(char));
-	}
-
-	printf("What do you want to enter ? (Choose 1, 2 or 3)\n1) Entropy\n2) Mnemonic\n3) Mnemonic + seed\n");
-	while (*ch != 'q') {
-		fgets(choice, 2, stdin);
-		switch (*choice) {
-		case '1':
-			viderBuffer();
-			printf("type of entropy :\n");
-			while (*ch != 'q') {
-
-				printf("choose 1 or 2 :\n1) binary\nor\n2) hexa\n");
-				fgets(choiceEntropy, 2, stdin);
-				switch (*choiceEntropy) {
-				case '1':
-					viderBuffer();
-					printf("enter entropy:\n");
-					fgets(entropy, 128, stdin);
-					//resultChar = findWord(/*fonction qui converti binaire en entiers*/, /*cheminFichier, result);
-					break;
-				case '2':
-					viderBuffer();
-					printf("enter entropy:\n");
-					fgets(entropy, 32, stdin);
-					//resultChar = findWord(/*fonction qui converti hexa en entiers*/, /*cheminFichier);s
-					break;
-				default:
-					printf("Choose 1 or 2 please\n");
-					break;
-				}
-			}			
-			break;
-		case '2':
-			viderBuffer();
-			printf("enter mnemonic :\n");
-			fgets(mnemonic, 500, stdin);
-			break;
-		case '3':
-			viderBuffer();
-			printf("enter mnemonic :\n");
-			fgets(mnemonic, 500, stdin);
-			printf("enter seed");
-			fgets(seed, 257, stdin);
-			break;
-		case 'q':
-			return 0;
-			break;
-		default:
-			printf("It is not 1, 2, 3 or q to quit !\n");
-		}
-
-	}
-
-	free(choice);
-	free(mnemonic);
-	free(seed);
-	free(resultChar);
-	free(resultInt);
-
-	return -1;
-}*/
